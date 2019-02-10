e622e7a2ff932ef8a2100a58eefa7f61b4240ff6
Mikio Kawakami <>
2019-02-10 22:25:01 +0900

    画像と音声メッセ対応1

--- main.py
+++ main.py
@@ -40,10 +40,7 @@ def callback():
         abort(400)
 
     return 'OK'
-    
-@handler.add(MessageEvent, message=(ImageMessage, AudioMessage))
-def handle_image_audio_message(event):
-    content = line_bot_api.get_message_content(event.message.id)
+
 
 #メッセージ判定イベントハンドラ
 @handler.add(MessageEvent, message=TextMessage)
@@ -70,6 +67,9 @@ def handle_message(event):
             event.reply_token,
             TextSendMessage(text="「お腹すいた」と言ってみよう！"+"\n"+"箱崎のお店をランダムに紹介するよ！！"))
 
+@handler.add(MessageEvent, message=(ImageMessage, AudioMessage))
+def handle_image_audio_message(event):
+    content = line_bot_api.get_message_content(event.message.id)
 
 if __name__ == "__main__":
 #    app.run()

fb1abb1047f997cc3b1525dd134eff0aae3038ee
Mikio Kawakami <>
2019-02-10 16:12:30 +0900

    if2

--- main.py
+++ main.py
@@ -45,9 +45,10 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="こんにちは"))
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text="よろしく〜"))
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="よろしく〜"))
 
 
 if __name__ == "__main__":

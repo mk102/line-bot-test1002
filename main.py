7b389652b681571525f6b4d0e0dba62377667c7d
Mikio Kawakami <>
2019-02-10 16:01:08 +0900

    if文を試す3

--- main.py
+++ main.py
@@ -50,10 +50,6 @@ def handle_follow(event):
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text = event.message.text
-    if text == "こんにちは":
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="こんにちは"))
     line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="よろしく〜"))

1ad7fbe4606d830621304b9b0241b40866713fa6
Mikio Kawakami <>
2019-02-10 15:57:22 +0900

    if文を試す

--- main.py
+++ main.py
@@ -37,12 +37,27 @@ def callback():
 
     return 'OK'
 
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
+# フォローイベントの場合の処理
+@handler.add(FollowEvent)
+def handle_follow(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text="よろしく〜"))
+        TextSendMessage(text='初めまして')
+    )
+
+# メッセージイベントの場合の処理
+# かつテキストメッセージの場合
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    text = event.message.text
+    if text == "こんにちは":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="よろしく〜"))
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="よろしく〜"))
 
 
 if __name__ == "__main__":

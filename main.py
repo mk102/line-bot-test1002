9e5e3840ae5319f80d3ee991a0cc50dfca3f801b
Mikio Kawakami <>
2019-02-10 17:57:45 +0900

    if2

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def handle_message(event):
     else:
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text="「お腹すいた」と言ってみよう！箱崎のお店をランダムに紹介するよ！！"))
+            TextSendMessage(text="「お腹すいた」と言ってみよう！"+"\n"+"箱崎のお店をランダムに紹介するよ！！"))
 
 
 if __name__ == "__main__":

a2701fccfaf6baf462fbcd7c8ff75103d9053c41
Mikio Kawakami <>
2019-02-11 10:17:27 +0900

    button template1

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def handle_message(event):
 
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(messages=buttons_template_message))
+            messages=buttons_template_message)
         print('error')
     else:
         line_bot_api.reply_message(

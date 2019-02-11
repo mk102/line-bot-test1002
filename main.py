d26494f160c917dd6cfd6bf6fe89f1dd266cd0fe
Mikio Kawakami <>
2019-02-11 11:01:46 +0900

    button template1

--- main.py
+++ main.py
@@ -80,7 +80,8 @@ def handle_message(event):
 
         line_bot_api.reply_message(event.reply_token, messages=buttons_template_message)
         #ここまで
-    #else:
+    else:
+        print('else')
         #line_bot_api.reply_message(
             #event.reply_token,
             #TextSendMessage(text="「お腹すいた」と言ってみよう！"+"\n"+"箱崎のお店をランダムに紹介するよ！！"))
